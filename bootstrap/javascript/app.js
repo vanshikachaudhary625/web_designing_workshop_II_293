a63ab9471d71e7ac4f566f9b673d85da54748d23
vanshikachaudhary625 <ch.vanshika492@gmail.com>
2026-04-09 19:49:47 +0530

    push

---
+++ bootstrap/javascript/app.js
@@ -0,0 +1,31 @@
+console.log("Vanshika");
+var name="Vanshika";
+var age=18;
+var isEmployed=true;
+console.log(name);
+console.log(age);
+console.log(isEmployed);
+//var age=50;
+//console.log(age);
+//Why this is dangeroud
+
+//JavaScript allows same variable name again
+if(true){
+    var age=30;
+
+}
+    console.log(age);
+
+const PI=3.14;
+console.log(PI);
+PI=3.14159;
+console.log(PI);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isEmployed);
+
+
+let result;
+console,log(result);
+console.log(typeof bigNumber);
+
---
+++ bootstrap/javascript/app1.js
@@ -0,0 +1,9 @@
+console.log("Vanshika");
+var name="Vanshika";
+var age=18;
+var isEmployed=true;
+console.log(name);
+console.log(age);
+console.log(isEmployed);
+let message = `Hello ${name}, you are ${age} years old.`;
+console.log(message);
